86071184abeb871d1c3ac906bca0d79f2ea2bc8e
Piyush <166606245+Piyush232005@users.noreply.github.com>
2026-02-07 19:58:54 +0530

    some changes in code

--- lambdafunction.py
+++ lambdafunction.py
@@ -1,40 +1,64 @@
 import json
+import urllib.request
 import boto3
-from boto3.dynamodb.conditions import Key
+from datetime import datetime
 from decimal import Decimal
 
+# ---- CONFIG ----
 TABLE_NAME = "crypto_price_history"
+SNS_TOPIC_ARN = "Paste your sns arn here"
+USD_TO_INR = Decimal("83")
 
+# ---- AWS Clients ----
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(TABLE_NAME)
 
+sns = boto3.client("sns")
+
+# ---- Helper ----
 def to_float(obj):
     if isinstance(obj, Decimal):
         return float(obj)
     return obj
 
+# ---- Lambda Handler ----
 def lambda_handler(event, context):
 
-    response = table.query(
-        KeyConditionExpression=Key("coin_id").eq("ETH"),
-        ScanIndexForward=False,
-        Limit=1
+    # Fetch ETH price from CoinGecko
+    url = "pasre ypur api url here"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read())
+
+    eth_usd = Decimal(str(data["ethereum"]["usd"]))
+    eth_inr = eth_usd * USD_TO_INR
+    timestamp = datetime.utcnow().isoformat()
+
+    # ---- Store in DynamoDB ----
+    table.put_item(
+        Item={
+            "coin_id": "ETH",
+            "timestamp": timestamp,
+            "price_usd": eth_usd,
+            "price_inr": eth_inr
+        }
     )
 
-    items = response.get("Items", [])
+    # ---- Send SNS Notification ----
+    message = f"""
+🚨 Ethereum Price Alert
 
-    if not items:
-        return {
-            "statusCode": 404,
-            "body": json.dumps({"data": None})
-        }
+USD Price: ${eth_usd}
+INR Price: ₹{eth_inr}
+Time: {timestamp}
+"""
 
-    item = items[0]
+    sns.publish(
+        TopicArn=SNS_TOPIC_ARN,
+        Subject="Ethereum Price Update",
+        Message=message
+    )
 
-    eth_usd = to_float(item["price_usd"])
-    eth_inr = to_float(item["price_inr"])
-    timestamp = item["timestamp"]
-
+    # ---- API Response ----
     return {
         "statusCode": 200,
         "headers": {
@@ -43,8 +67,8 @@ def lambda_handler(event, context):
         },
         "body": json.dumps({
             "data": {
-                "eth_usd": eth_usd,
-                "eth_inr": eth_inr,
+                "eth_usd": to_float(eth_usd),
+                "eth_inr": to_float(eth_inr),
                 "timestamp": timestamp
             }
         })
